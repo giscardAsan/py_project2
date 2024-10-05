551f88351610a201fd2bb69f4d7416485956f0a0
giscardAsan <asantegiscard23@gmail.com>
2024-10-05 00:45:58 +0000

    commit

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+   
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -126,3 +126,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+  
--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -4,6 +4,6 @@ from .models import post
 # Create your views here.
 
 def posts_list(request):
-    posts = post.objects.all()
-    return render(request, 'posts/posts_list.html', { 'posts': post})
+    posts = post.objects.all().order_by('date')
+    return render(request, 'posts/posts_list.html', { 'posts': posts})
  
